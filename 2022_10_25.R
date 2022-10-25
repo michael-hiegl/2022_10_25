cb7f84fda6170f622607e4052176b8bd9b4e58b8
michael-hiegl <100510019+michael-hiegl@users.noreply.github.com>
2022-10-25 09:44:50 +0200

    Add files via upload

---
+++ 2022_10_25.R
@@ -0,0 +1,23 @@
+
+
+
+
+
+out <- function(Fruits)
+{
+  for(Fruit in Fruits)
+  {
+    print(nchar(Fruit))
+    print(Fruit)
+  }
+}
+
+
+
+Früchte <- c("Apfel", "Zitrone", "Quitte")
+
+out(Früchte)
+
+
+
+
